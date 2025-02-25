0c421255d4e9633afd2ab15459a98d637b9d3ea3
Pham Dang Khoa <156926024+phamdangkhoagh@users.noreply.github.com>
2025-02-25 21:07:01 +0700

    feat(api): user login with email and password

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,15 @@ use App\Http\Controllers\API\AuthController;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::post('/api/register', [AuthController::class, 'register']); 
-// });
-
 Route::post('/register', [AuthController::class, 'register']); 
-Route::post('/login', function (Request $request) {
-    return response()->json([
-        'message' => 'User logged in successfully!',
-    ], 200, ['Content-Type' => 'application/json']);
+Route::post('/login', [AuthController::class, 'login']); 
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
 });
+
+// Route::post('/login', function (Request $request) {
+//     return response()->json([
+//         'message' => 'User logged in successfully!',
+//     ], 200, ['Content-Type' => 'application/json']);
+// });
